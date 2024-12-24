21b1d51ce2994b127d698d46e939f67693e39dbb
cochengannan <cochengannanofficial@gmail.com>
2024-12-24 14:54:45 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dashboard||CSC</title>
+	<link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
+	<link rel="stylesheet" href="../css/dashboard.css">
+</head>
+<body>
+	<input type="checkbox" name="" id="menu-toggle">
+	<div class="sidebar">
+		<div class="sidebar-container">
+			<div class="brand">
+				<h2>
+					<span class="lab la-phoenix-squadron"></span>
+					Computer Software College
+				</h2>
+			</div>
+			<div class="sidebar-avatar">
+				<div>
+					<img src="../images/login_ava.png" alt="image not found">
+				</div>
+				<div class="avatar-info">
+					<div class="avatar-text">
+						<h4>Sudharshan Selvam</h4>
+						<small>+91 9566026610</small>
+					</div>
+					<span class="las la-angle-double-down"></span>
+				</div>
+			</div>
+			<div class="sidebar-menu">
+				<ul>
+					<li>
+						 <a href=""class="active">
+						 <span class="las la-adjust"></span>
+						 <span>Dashboard</span>
+						</a>
+					</li>
+					<li>
+						 <a href="">
+						 <span class="las la-chalkboard-teacher"></span>
+						 <span>Staff</span>
+						</a>
+					</li>
+					<li>
+						 <a href="">
+						 <span class="las la-graduation-cap"></span>
+						 <span>Student</span>
+						</a>
+					</li>
+					<li>
+						 <a href="">
+						 <span class="lab la-studiovinari"></span>
+						 <span>Enquiry</span>
+						</a>
+					</li>
+					<li>
+						 <a href="">
+						 <span class="las la-user-tie"></span>
+						 <span>Users</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="sidebar-card">
+				<div class="side-card-icon">
+					<span class="lab la-codiepie"></span>
+				<div>
+					<h4>Make AdSense</h4>
+					<p>Add ads to your videos</p>
+				</div>
+				<button class="btn btn-main btn-block"><span class="las la-plus">Create</span></button>
+			</div>
+		</div>
+	</div>
+	<div class="main-content">
+		<header>
+			<div class="header-title-wrapper">
+				<label for="">
+					<span class="las la-bars"></span>
+				</label>
+				<div class="header-title">
+					<h1>Analytics</h1>
+					<p>Display analytics about your channel<span class="las la-chart-line"></span></p>
+				</div>
+			</div>
+			<div class="header-action">
+				<button> 
+					<span class="las la-video"></span> 
+					Upload
+				</button>
+			</div>
+		</header>
+		<main>
+
+			<section>
+				<h3 class="selction-head"></h3>
+			</section>
+				<div class="analytics">
+				<div class="analytic">
+					<div class="analytic-icon">
+						<span class="las la-eye">
+						</span>
+					</div>
+					<div class="alalytic-info">
+						<h4>Total views</h4>
+						<h1>10.3M</h1>
+					</div>
+				</div>
+
+			</div>
+			<div class="analytics">
+				<div class="analytic">
+					<div class="analytic-icon">
+						<span class="las la-clock">
+						</span>
+					</div>
+					<div class="alalytic-info">
+						<h4>Watch Time (hrs)</h4>
+						<h1>20.9k <small><span class="text-danger"></span>5%
+						</small>
+						</h1>
+					</div>
+				</div>
+
+			</div>
+			<div class="analytics">
+				<div class="analytic">
+					<div class="analytic-icon">
+						<span class="las la-users">
+						</span>
+					</div>
+					<div class="alalytic-info">
+						<h4>Subscribers</h4>
+						<h1>1.3k <small><span class="text-success"></span>12%
+						</small>
+						</h1>
+					</div>
+				</div>
+
+			</div>
+			<div class="analytics">
+				<div class="analytic">
+					<div class="analytic-icon">
+						<span class="las la-heart">
+						</span>
+					</div>
+					<div class="alalytic-info">
+						<h4>Total likes</h4>
+						<h1>3.4M</h1>
+					</div>
+				</div>
+
+			</div>
+			<div class="block-grid">
+				<div class="revenue-card">
+					<img src="-----" alt=""/>
+					<div class="rev-info">
+							<h3>Ralph Anderson</h3>
+				<h1>3.5M<small>Subscribers</small></h1>
+			
+					</div>
+					<div class="rev-sum">
+						<h4>Total income</h4>
+						<h2>$70,859</h2>
+					</div>
+			</div>		
+			</div>
+
+<section>
+
+	<div class="block-grid">
+				<div class="revenue-card">
+					<img src="-----" alt=""/>
+					<div class="rev-info">
+							<h3>Ralph Anderson</h3>
+				<h1>3.5M<small>Subscribers</small></h1>
+			
+					</div>
+					<div class="rev-sum">
+						<h4>Total income</h4>
+						<h2>$70,859</h2>
+					</div>
+			</div>		
+			</div>
+</section>
+		</main>
+	</div>
+</body>
+</html>
+
+
